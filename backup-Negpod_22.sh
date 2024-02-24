b1de281decc5c50a83e3a217a768b7ffce778522
TammyBriggs <t.briggs1@alustudent.com>
2024-02-24 12:21:03 -0800

    Update to backup file

--- backup-Negpod_22.sh
+++ backup-Negpod_22.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 
-#configuration
-
+# Configuration
 HOST="64293e56bc62.3a2627c1.alu-cod.online"
 USERNAME="64293e56bc62"
 PASSWORD="328d3b338a4ced526c9a"
-
 
 # Destination location on the remote server
 DESTINATION="/summative/1023-2024j"
@@ -16,11 +14,10 @@ if [ ! -d "$DIRECTORY" ]; then
     exit 1
 fi
 
-#Backup command
-rsync -avz -e "sshpass -p $PASSWORD ssh -o StrictHostKeyChecking=no" "$DIRECTORY" "$USERNAME@$HOST:$DESTINATION"
+# Backup command using scp
+sshpass -p "$PASSWORD" scp -o StrictHostKeyChecking=no -r "$DIRECTORY" "$USERNAME@$HOST:$DESTINATION"
 
-
-# Check rsync exit status
+# Check scp exit status
 if [ $? -eq 0 ]; then
     echo "Backup completed successfully."
 else
